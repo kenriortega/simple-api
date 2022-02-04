42112fce8a28fd7292e1a385567cf20a4fe297af
kenriortega <kenriortega@gmail.com>
2022-02-03 22:44:38 -0500

    ref: change code like this post `https://pace.dev/blog/2018/05/09/how-I-write-http-services-after-eight-years.html`

--- main.go
+++ main.go
@@ -22,32 +22,28 @@ func init() {
 	runtime.GOMAXPROCS(numcpu)
 }
 
-const (
-	DB    = "challenge"
-	USERS = "users"
-)
-
-// User ...
-type User struct {
-	ID          primitive.ObjectID `json:"id,omitempty" bson:"_id"`
-	FirstName   string             `json:"first_name,omitempty"`
-	LastName    string             `json:"last_name,omitempty"`
-	Email       string             `json:"email,omitempty"`
-	Phone       string             `json:"phone,omitempty"`
-	JobTitle    string             `json:"job_title,omitempty"`
-	Domain      string             `json:"domain,omitempty"`
-	URL         string             `json:"url,omitempty"`
-	PaymentCard string             `json:"payment_card,omitempty"`
-}
-
 func main() {
 	r := http.NewServeMux()
-	h := HandlerApp{m: MongoDbClient()}
+	api := New(r)
+	api.routes()
+	api.Start()
+}
 
-	r.HandleFunc("/all", h.ReadUsers)
+type server struct {
+	db     *mongo.Client
+	router *http.ServeMux
+}
 
+func New(r *http.ServeMux) *server {
+	return &server{
+		db:     nil,
+		router: r,
+	}
+}
+
+func (s *server) Start() {
 	srv := &http.Server{
-		Handler: r,
+		Handler: s.router,
 		Addr:    fmt.Sprintf(":%s", os.Getenv("PORT")),
 		// Good practice to set timeouts to avoid Slowloris attacks.
 		WriteTimeout: time.Second * 15,
@@ -61,36 +57,21 @@ func main() {
 	}
 
 }
-
-// Handlers
-// HandlerApp
-type HandlerApp struct {
-	m *mongo.Client
+func (s *server) routes() {
+	s.router.HandleFunc("/all", s.handleUsers())
 }
 
-// ReadUsers ...
-func (h *HandlerApp) ReadUsers(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		users, err := h.FetchAllUser(r.Context())
-		if err != nil {
-			fmt.Fprintf(w, err.Error())
-		}
-		err = json.NewEncoder(w).Encode(users)
-		if err != nil {
-			fmt.Fprintf(w, err.Error())
-		}
-	}
-}
+func (s *server) handleUsers() http.HandlerFunc {
 
-// DB
-//Used to execute client creation procedure only once.
-var mongoOnce sync.Once
+	var (
+		init sync.Once
 
-// MongoDbClient return an unique instance of mongodb server
-func MongoDbClient() *mongo.Client {
-	var clientInstance *mongo.Client
+		DB    = "challenge"
+		USERS = "users"
+	)
+
 	//Perform connection creation operation only once.
-	mongoOnce.Do(func() {
+	init.Do(func() {
 		// Set client options
 		clientOptions := options.Client().ApplyURI(
 			os.Getenv("DATABASE_URL"),
@@ -107,26 +88,41 @@ func MongoDbClient() *mongo.Client {
 			log.Fatal(fmt.Sprintf("mongodb: %v", err))
 			panic(err)
 		}
-		clientInstance = client
+		s.db = client
 	})
-	return clientInstance
-}
 
-// FetchAllUser ...
-func (h *HandlerApp) FetchAllUser(ctx context.Context) ([]map[string]interface{}, error) {
-	filter := bson.D{{}} //bson.D{{}} specifies 'all documents'
-
-	collection := h.m.Database(DB).Collection(USERS)
-	//Perform Find operation & validate against the error.
-	cur, err := collection.Find(ctx, filter)
-	if err != nil {
-		return nil, err
+	// User ...
+	type User struct {
+		ID          primitive.ObjectID `json:"id,omitempty" bson:"_id"`
+		FirstName   string             `json:"first_name,omitempty"`
+		LastName    string             `json:"last_name,omitempty"`
+		Email       string             `json:"email,omitempty"`
+		Phone       string             `json:"phone,omitempty"`
+		JobTitle    string             `json:"job_title,omitempty"`
+		Domain      string             `json:"domain,omitempty"`
+		URL         string             `json:"url,omitempty"`
+		PaymentCard string             `json:"payment_card,omitempty"`
 	}
-	users := make([]map[string]interface{}, cur.RemainingBatchLength())
-	if err := cur.All(ctx, &users); err != nil {
-		panic(err)
+	// HandleFunc ...
+	return func(w http.ResponseWriter, r *http.Request) {
+		filter := bson.D{{}}
+
+		collection := s.db.Database(DB).Collection(USERS)
+		cur, err := collection.Find(r.Context(), filter)
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
+		}
+		users := make([]map[string]interface{}, cur.RemainingBatchLength())
+		if err := cur.All(r.Context(), &users); err != nil {
+			fmt.Fprintf(w, err.Error())
+		}
+
+		if r.Method == "GET" {
+
+			err = json.NewEncoder(w).Encode(users)
+			if err != nil {
+				fmt.Fprintf(w, err.Error())
+			}
+		}
 	}
-
-	return users, nil
-
 }
