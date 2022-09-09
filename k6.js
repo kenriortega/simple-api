077d30ea790c54633457f49dd3350832f5f0dd12
Kenrique Ortega <53656046+kenriortega@users.noreply.github.com>
2022-09-09 08:18:34 -0400

    Update k6.js

--- k6.js
+++ k6.js
@@ -4,7 +4,6 @@ export const options = {
     vus: 1000, // virtual users
     duration: '30s',
 };
-const csv = open('/home/qwerty/github.com/dofleini/data-importer/examples/datafiles/contacts_lite.csv');
 
 export default function () {
 
